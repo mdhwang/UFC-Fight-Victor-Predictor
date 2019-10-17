5a1812ced85c1d626aad4eddc1fc63c8417fa466
mdhwang <mattdhwang@gmail.com>
2019-10-17 10:26:08 -0700

    updated logging feature for multiple models

--- log.py
+++ log.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def log(cols,RF_FeatImp,RF_Acc,RF_Params):
+def log(cols,log_info):
     now = datetime.now()
     dt_string = now.strftime("%m-%d-%Y_%H:%M:%S")
 
@@ -17,16 +17,21 @@ def log(cols,RF_FeatImp,RF_Acc,RF_Params):
         file.write('\n')
     file.write('\n')
     file.write('\n')
-    file.write('---------RANDOM FOREST-----------')
-    file.write('\n')
-    file.write(RF_Params)
-    file.write('\n')
-    file.write('\n')
-    file.write('RF Accuracy : {} \n'.format(round(RF_Acc,2)))
-    file.write('\n')
-    for each in RF_FeatImp:
-        file.write(str(each))
-        file.write("\n")
+    for each in log_info:
+        file.write('---------{}-----------'.format(each))
+        file.write('\n')
+        file.write(log_info[each][0])
+        file.write('\n')
+        file.write('\n')
+        file.write('{} Accuracy : {} \n'.format(each,round(log_info[each][1],2)))
+        file.write('\n')
+        for feat in log_info[each][2]:
+            file.write(str(feat))
+            file.write('\n')
+        file.write('\n')
+        file.write('\n')
+        file.write('\n')
+        file.write('\n')
     file.close()
     return "Logged Successfully"
 
